5eaec9ce0afe6b12a8ae2cc7bbc2b53db4da7e78
simar <simarsjohar@gmail.com>
2021-05-23 15:46:49 +0100

    chapter 6 attempted , excerices not completed for this chapter

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from rango.models import Category, Page
 # Register your models here.
-from tango_with_django_project.rango.models import PageAdmin
+from rango.models import PageAdmin
 
+
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+# Update the registration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Category)
 admin.site.register(PageAdmin)
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,12 +1,23 @@
 from django.db import models
-from django.db import admin
 
+from django.template.defaultfilters import slugify
+
+
+slug = models.SlugField(unique=True)
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
@@ -19,7 +30,7 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
 
 
-class PageAdmin(admin.ModelAdmin):
+class PageAdmin(models.Model):
     list_display = ('title',
                     'category', 'url')
 
